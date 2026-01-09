20ac776f205f43173f4bac328bfc444a533c2e9f
Tyasweningas <tyasweningas@gmail.com>
2026-01-09 07:30:54 +0700

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,22 @@ import Footer from "@/components/global/footer";
 import ProductCard from "@/components/global/product-card-landing";
 import NavbarLanding from "@/components/global/navbar-landing";
 import Navbar from "@/components/global/navbar";
+import LandingPage from "./landing-page/page";
+import { redirect } from 'next/navigation';
 
+
+// export default function Home() {
+//   return (
+//     <>
+//       redirect('/landing-page');
+//     </>
+//   )
+// }
+
+// app/page.tsx
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-    </>
-  )
+  redirect("/landing-page");
+  return null;
 }
+
